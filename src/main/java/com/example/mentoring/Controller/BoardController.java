7231fcaacd2b2da2b9ae21ccfee1603b4c7fc7a7
lsuinl <su96in43@naver.com>
2022-07-16 23:38:52 +0900

    2week:

---
+++ src/main/java/com/example/mentoring/Controller/BoardController.java
@@ -0,0 +1,54 @@
+package com.example.mentoring.Controller;
+
+import com.example.mentoring.entity.Board;
+import com.example.mentoring.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller 는 클라이언트(사용자)의 요청을 받는 클래스입니다.
+ * Controller에서는 서비스 클래스 (기능 구현된 클래스) 를 불러서 사용자의 요청을 처리합니다.
+ *
+ * @ Controller 는 템플릿 엔진(JSP, Thymeleaf 등등) 사용할 때 주로 쓰이고,
+ * @ RestController 는 API 서버를 만들 때 주로 사용됩니다. 저희는 API 서버를 만드니 RestController 로 진행합니다.
+ */
+
+@RequiredArgsConstructor // 생성자
+@RestController
+public class BoardController {
+    private final BoardService boardService;
+
+    // 게시글 전체 조회
+    @GetMapping("/api/boards")
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(boardService.findAllBoard(), HttpStatus.OK);
+    }
+
+    // 게시글 단건 조회
+    @GetMapping("/api/boards/{id}")
+    public ResponseEntity<?> findBoard(@PathVariable("id") int id) {
+        return new ResponseEntity<>(boardService.findBoard(id), HttpStatus.OK);
+    }
+
+    // 게시글 작성
+    @PostMapping("/api/boards")
+    public ResponseEntity<?> writeBoard(@RequestBody Board board) {
+        return new ResponseEntity<>(boardService.writeBoard(board), HttpStatus.CREATED);
+    }
+
+    // 게시글 수정
+    @PutMapping("/api/boards/{id}")
+    public ResponseEntity<?> editBoard(@PathVariable("id") int id, @RequestBody Board board) {
+        return new ResponseEntity<>(boardService.editBoard(id, board), HttpStatus.OK);
+    }
+
+
+    // 게시글 삭제
+    @DeleteMapping("/api/boards/{id}")
+    public ResponseEntity<?> deleteBoard(@PathVariable int id) {
+        boardService.deleteBoard(id);
+        return new ResponseEntity<>("게시글 삭제 완료", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/mentoring/service/BoardService.java
@@ -0,0 +1,70 @@
+package com.example.mentoring.service;
+
+import com.example.mentoring.entity.Board;
+import com.example.mentoring.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Service 는 기능을 구현하는 것에 초점을 둔 클래스입니다.
+ * Repository 를 불러와서, 데이터베이스에 데이터를 넣거나 혹은 가져와서 기능을 구현합니다.
+ * <p>
+ * Service 클래스는 Controller 에서 불러와서 사용합니다.
+ */
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    // 게시글 전체 조회
+    @Transactional(readOnly = true)
+    public List<Board> findAllBoard() {
+        Board board = boardRepository.findById(1).get();
+
+        return boardRepository.findAll();
+    }
+
+    // 게시글 단건 조회
+    @Transactional(readOnly = true)
+    public Board findBoard(int id) {
+        return boardRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    }
+
+    // 게시글 작성
+    @Transactional
+    public Board writeBoard(Board boardReq) {
+        Board board = new Board(boardReq.getTitle(), boardReq.getContent(), boardReq.getWriter());
+        boardRepository.save(board);
+        return board;
+    }
+
+
+    // 게시글 수정
+    @Transactional
+    public Board editBoard(int id, Board updateBoard) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException();
+        });
+
+        board.setTitle(updateBoard.getTitle());
+        board.setContent(updateBoard.getContent());
+        return board;
+    }
+
+
+    // 게시글 삭제
+    @Transactional
+    public void deleteBoard(int id) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException();
+        });
+
+        boardRepository.deleteById(id);
+    }
+}
+
